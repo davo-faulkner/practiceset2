45cdc0a1c10cd6f87d86d2270f3911c9bd9145fd
Davo Faulkner <d@davo.co>
2016-08-24 16:46:25 -0500

    Paste gist and run (good)

--- app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.practiceset2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Test Comment for Git
@@ -13,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7 * 8;
+
+        int actualHours = weekday;
+        actualHours = actualHours + weekend * 2;
+        int solution = optimalHours - actualHours;
+        display(solution);
+    }
+
+    public void display (int i) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(""+i);
     }
 }
